0e1f430d6bd5bd317f829a91bd729bc2c994b308
TAK <infernal-slam@hotmail.com>
2016-04-02 03:27:23 +0700

    start

--- index.js
+++ index.js
@@ -2,10 +2,22 @@
 var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
-// mongoose.connect('mongodb://localhost:27017/db_test')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static('public'))
+var macaddress = require('macaddress')
+// macaddress.one(function (err, mac) {
+//   console.log('Mac address for this host: %s', mac)
+// })
+var data = []
+macaddress.all(function (err, all) {
+  console.log(JSON.stringify(all, null, 2))
+  data.push(JSON.stringify(all, null, 2))
+})
+app.get('/data', function (req, res) {
+  res.send(data)
+})
+// mongoose.connect('mongodb://localhost:27017/db_test')
 // var data = require('./data.route.js')
 // app.use('/api/data', data)
 // connect zone port//
--- public/app.js
+++ public/app.js
@@ -1,8 +1,19 @@
-/* global angular alert*/
+/* global angular*/
 angular.module('todoApp', [])
   .controller('TodoListController', function ($http) {
     var todoList = this
+    todoList.macaddress = []
     todoList.sub = function () {
-      alert('Yes')
+      console.log('Get mac address')
+      GetMac()
+    }
+    function GetMac () {
+      $http.get('/data')
+        .then(function success (response) {
+          console.log(response)
+          todoList.macaddress.push(response)
+        }, function error (response) {
+          console.log(response)
+        })
     }
   })
